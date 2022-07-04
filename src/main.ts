095e491fe2d11116b0c184d2493fada73343ddc7
John Smith <rintran720@gmail.com>
2022-07-05 06:04:08 +0700

    feat: compression

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import * as compression from 'compression';
 import * as cookieParser from 'cookie-parser';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
@@ -8,6 +9,12 @@ async function bootstrap() {
   app.use(helmet());
   app.enableCors();
   app.use(cookieParser());
+  app.use(
+    compression({
+      level: 6,
+      threshold: 100 * 1000, // bytes
+    }),
+  );
 
   const PORT = process.env.PORT || 4860;
   await app.listen(PORT);
